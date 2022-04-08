569d45ef30ce0ada92ecfa663ce7e3af2a7c967e
coco <corentin.barault@gmail.com>
2022-04-08 02:36:46 +0200

    write to file

--- src/projetCrypto/main.java
+++ src/projetCrypto/main.java
@@ -1,5 +1,6 @@
 package projetCrypto;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -12,30 +13,33 @@ public class main {
     public static void main (String[] args) throws IOException{
     	Loader loader = new Loader();
         GenerateurCle g = new GenerateurCle();
-        byte[] octet = loader.Charger("hello.txt");
-        
-        /*String a = "sdfghujklm";
-        System.out.println(a);
-        String b = g.crypte(a);
-        System.out.println(b);
-        String c = g.crypte(b);
-        System.out.println(c);*/
+        byte[] octet = loader.Charger("active3d.png");
     	
         byte[] b = g.crypteByteArray(octet);
     	
-        for (int i = 0; i < b.length; i++) {
+        for (int i = 0; i < 10; i++) {
         	System.out.println(
         			"b[" + i + "] = " +((int)b[i] < 11  ? "  " : "") +
         			b[i] + " , " +" character=(" + (char)b[i] + ")");
         }
         
-        byte[] c = g.crypteByteArray(b);
+        try (FileOutputStream fos = new FileOutputStream("testcrypte")) {
+     	   fos.write(b);
+     	   //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+     	}
         
-        for (int i = 0; i < c.length; i++) {
+        byte[] c = g.crypteByteArray(loader.Charger("testcrypte"));
+        
+        for (int i = 0; i < 10; i++) {
         	System.out.println(
         			"c[" + i + "] = " +((int)c[i] < 11  ? "  " : "") +
         			c[i] + " , " +" character=(" + (char)c[i] + ")");
         }
+        
+        try (FileOutputStream fos = new FileOutputStream("testdecrypte.png")) {
+        	   fos.write(c);
+        	   //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+        	}
         
    }
             
